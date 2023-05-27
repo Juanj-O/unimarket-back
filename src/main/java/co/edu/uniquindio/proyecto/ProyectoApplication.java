2a81f10ed581afefcf9980063d32e59fbbaff3a0
Juanj-O <juan.osorio@celuweb.com>
2023-05-27 18:49:38 -0500

    Cambios app

--- src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -1,9 +1,25 @@
 package co.edu.uniquindio.proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class ProyectoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProyectoApplication.class, args);
     }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://unimarket-back-production.up.railway.app/").allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
 }
+
