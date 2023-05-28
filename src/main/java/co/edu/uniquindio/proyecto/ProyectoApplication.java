39cf861efbc348289a1b0759bd9d7a2b066a0df5
Juanj-O <juan.osorio@celuweb.com>
2023-05-28 02:19:04 -0500

    Cors config

--- src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -10,23 +10,5 @@ public class ProyectoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProyectoApplication.class, args);
     }
-
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://unimarket-back-production.up.railway.app", "http://unimarket-back-production.up.railway.app/api/")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("header1", "header2", "header3")
-                        .exposedHeaders("header1", "header2")
-                        .allowCredentials(true).maxAge(3600);
-            }
-
-        };
-    }
 }
 
--- src/main/java/co/edu/uniquindio/proyecto/seguridad/config/CorsConfig.java
+++ src/main/java/co/edu/uniquindio/proyecto/seguridad/config/CorsConfig.java
@@ -1,16 +1,14 @@
-package co.edu.uniquindio.proyecto.config;
+package co.edu.uniquindio.proyecto.seguridad.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://unimarket-back-production.up.railway.app", "http://unimarket-back-production.up.railway.app/api/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("*");
+
     }
 }
